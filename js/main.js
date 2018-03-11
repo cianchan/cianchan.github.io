5310c648590189ee79d91b5f385d907d7d8ac6f9
franco <chenhuifan.gg@gmail.com>
2018-03-11 23:07:51 +0800

    ui

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+    //Initialize filterizr with default options
+    $('.filtr-container').filterizr(
+      options = {
+         animationDuration: 0.25,
+         filter: '1', // Initial filter
+      }
+    );
+});
+
+$('.filter__button').on('click', function(){
+	$('.filter__button').removeClass('filter__button--active');
+	$(this).addClass('filter__button--active');
+});
