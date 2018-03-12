d02c34b3accaf8dd738d85520e9eadf1c73c349f
franco <chenhuifan.gg@gmail.com>
2018-03-12 12:26:17 +0800

    loading

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
+// filterizr
 $(function() {
     //Initialize filterizr with default options
     $('.filtr-container').filterizr(
       options = {
          animationDuration: 0.25,
          filter: '1', // Initial filter
-      	 layout: 'sameHeight',
+      	 layout: 'packed',
       }
     );
 });
 
+
+// tabs toggle class
 $('.filter__button').on('click', function(){
 	$('.filter__button').removeClass('filter__button--active');
 	$(this).addClass('filter__button--active');
